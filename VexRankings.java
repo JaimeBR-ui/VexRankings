9338b7c7a50da14139d546267103e8dacf4a752f
JaimeBR-ui <bambas@knights.ucf.edu>
2019-12-08 02:33:11 -0500

    Added a comparator object to sort them by ranking and team number.

--- VexRankings.java
+++ VexRankings.java
@@ -15,7 +15,7 @@ public class VexRankings
 {
      private static final String gradeLevel = "College";
      private static final String season = "Tower Takeover";
-     private static final int numberOfRankings = 20;
+     private static final int numberOfRankings = 50;
 
      private static String vexDBFile = "./data/vexdb_rankings.json";
      private static String robotEventsFile = "./data/robot_events_rankings.json";
@@ -32,22 +32,28 @@ public class VexRankings
           URLWritter.writeToFile(robotEventsURL, robotEventsFile);
 
           // Read json files and return head of linked list.
-          Team robotEvents = JsonReader.getRobotEventsScores(robotEventsFile,
-                                                            numberOfRankings);
-          Team vexDB = JsonReader.getVexDBScores(vexDBFile, numberOfRankings);
+          Team robotEvents = JsonReader.getRobotEventsScores(robotEventsFile);
+          Team vexDB = JsonReader.getVexDBScores(vexDBFile);
 
           System.out.println("     Reading files...");
-          Set<Team> teams = new TreeSet<>();
+          Map<String, Team> teams = new TreeMap<>();
 
           while (robotEvents != null)
           {
-               teams.add(robotEvents);
+               Team t = teams.get(robotEvents.plateNumber());
+
+               if (t == null || t.score() < robotEvents.score())
+                    teams.put(robotEvents.plateNumber(), robotEvents);
+
                robotEvents = robotEvents.next;
           }
 
           while (vexDB != null)
           {
-               teams.add(vexDB);
+               Team t = teams.get(vexDB.plateNumber());
+
+               if (t == null || t.score() < vexDB.score())
+                    teams.put(vexDB.plateNumber(), vexDB);
                vexDB = vexDB.next;
           }
 
@@ -89,14 +95,31 @@ public class VexRankings
           else
                return null;
 
+          System.out.println(url);
           return url;
      }
 
-     protected static void printRankings(Set<Team> teams)
+     protected static void printRankings(Map<String, Team> teams)
      {
           int i = 1;
+          Set<String> set = teams.keySet();
 
-          for (Team t : teams)
+          List<Team> list = new ArrayList<>();
+
+          for (String s : set)
+               list.add(teams.get(s));
+
+          Comparator<Team> scoreOrder =  new Comparator<Team>()
+          {
+               public int compare(Team t1, Team t2)
+               {
+                    return t2.score() - t1.score();
+               }
+          };
+
+          Collections.sort(list, scoreOrder);
+
+          for (Team t : list)
           {
                t.setRank(i);
 
@@ -108,7 +131,7 @@ public class VexRankings
                for (int j = 0; j < 6 - t.plateNumber().length(); j++)
                     System.out.print(" ");
 
-               System.out.println("\t" + t.score());
+               System.out.println("\t" + t.score() + "\t" + t.name());
                if (++i > numberOfRankings)
                     break;
           }
